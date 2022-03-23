dbf55f6e6e5fee0aa377903b545b6e9f9d55e82d
Sergio-Jurado <juradotrillosergio1207@gmail.com>
2022-03-23 12:58:22 +0100

    Este es el quinto ejercicio

---
+++ Ejercicios/ejercicio5.py
@@ -0,0 +1,12 @@
+#Teniendo estas dos listas escribe un programa que devuelva una lista que solo tenga los elementos comunes sin duplicar
+lista1=[1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89]
+lista2=[1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13]
+lista3=[]
+elemento = 0
+for elemento in lista1:
+    if elemento in lista2:
+        if elemento not in lista3:
+            lista3.append(elemento)
+
+print(lista3)
+    
