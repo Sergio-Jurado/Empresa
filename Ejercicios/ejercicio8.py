c0d7aa4981944d11b597e5662f654ead862afd86
Sergio-Jurado <juradotrillosergio1207@gmail.com>
2022-04-06 13:44:55 +0200

    Este es el ejercicio 8

---
+++ Ejercicios/ejercicio8.py
@@ -0,0 +1,13 @@
+# Escribir en un fichero de texto
+"""
+mensaje = input("Escriba el mensaje que desa escribir dentro de un archivo txt: ")
+with open("archivo.txt", "w" ) as f:
+    f.write(mensaje)
+"""
+mensaje = input("Escriba el mensaje que desa escribir dentro de un archivo txt: ")
+f = open("archivo.txt", "w")
+f.write(mensaje)
+f.close()
+with open("archivo.txt", "r" ) as f:
+    lectura = f.read()
+print(lectura)
